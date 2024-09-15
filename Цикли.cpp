66b892a350666e4939c8ff4f63b5aa013a811df2
DeepCool <DeepCool@DESKTOP-O8K62PQ>
2024-09-15 13:11:32 +0300

    Add project files.

---
+++ Цикли.cpp
@@ -0,0 +1,99 @@
+﻿#include <iostream>
+using namespace std;
+
+int main() 
+{
+	//1
+//    int a;
+//    cout << "Enter an integer a: ";
+//    cin >> a;
+//
+//    if (a > 500) {
+//        cout << "The number must be less than or equal to 500" << endl;
+//    }
+//    else {
+//        int sum = 0;
+//        for (int i = a; i <= 500; i++) {
+//            sum += i;
+//        }
+//
+//        cout << "Sum of integers from " << a << " to 500: " << sum << endl;
+//    }
+// 
+    //2
+        //int x, y;
+        //cout << "Enter integer x: ";
+        //cin >> x;
+        //cout << "Enter integer y (exponent): ";
+        //cin >> y;
+
+        //if (y == 0) {
+        //    cout << x << " raised to the power of " << y << " is: 1" << endl;
+        //}
+        //else {
+        //    int result = 1;
+        //    for (int i = 0; i < y; i++) {
+        //        result *= x; 
+        //    }
+
+        //    cout << x << " raised to the power of " << y << " is: " << result << endl;
+        //}
+
+        /*    3
+        int n = 1000; 
+        int sum = (n * (1 + n)) / 2; 
+        int average = sum / n; 
+        cout << "Arithmetic mean of all integers from  1 to 1000 : " << average << endl;*/
+        
+
+
+        //    4
+        //int a;
+        //long long product = 1; 
+
+        //cout << "Enter the value of a: ";
+        //cin >> a;
+
+        //if (a < 1 || a > 20) {
+        //    cout << "Error: a must be between 1 and 20." << endl;
+        //}
+
+        //for (int i = a; i <= 20; ++i) {
+        //    product *= i;
+        //}
+
+        //cout << "The product of all integers from " << a << " to 20 is: " << product << endl;
+
+            //5
+        //int k;
+
+        //cout << "Enter option number: ";
+        //cin >> k;
+
+        //for (int i = 2; i <= 10; ++i) {
+        //    cout << k << " x " << i << " = " << k * i << endl;
+        //}
+
+
+    //Домашнє завдання 2
+    //1
+        //int count = 0; 
+
+        //for (int number = 100; number <= 999; ++number) {
+        //    int hundreds = number / 100;         
+        //    int tens = (number / 10) % 10;       
+        //    int ones = number % 10;             
+
+        //    if (hundreds == tens || hundreds == ones || tens == ones) {
+        //        count++;
+        //    }
+        //}
+
+        //cout << "The number of numbers in the range from 100 to 999 contains two digits: " << count << endl;
+
+    }
+
+
+
+
+
